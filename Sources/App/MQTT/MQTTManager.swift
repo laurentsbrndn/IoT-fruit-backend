aebe433b831ac275958efbcda6eb5445acb8db80
laurentsbrndn <brandonvikario607@gmail.com>
2026-08-19 23:14:06 +0700

    move the sensitive file to .env

--- Sources/App/MQTT/MQTTManager.swift
+++ Sources/App/MQTT/MQTTManager.swift
@@ -14,11 +14,11 @@ final class MQTTManager {
     }
 
     func start() throws {
-        let host = Environment.get("MQTT_HOST") ?? "dfc14af1.ala.asia-southeast1.emqxsl.com"
-        let port = Int(Environment.get("MQTT_PORT") ?? "8883") ?? 8883
-        let username = Environment.get("MQTT_USER") ?? "iot_tracker"
-        let password = Environment.get("MQTT_PASSWORD") ?? "884fd935-7965-4c19-8d59-973fc5fa11b6"
-        let useTLS = (Environment.get("MQTT_TLS") ?? "true").lowercased() == "true" || port == 8883
+        let host = Environment.get("MQTT_HOST") ?? "localhost"
+        let port = Int(Environment.get("MQTT_PORT") ?? "1883") ?? 1883
+        let username = Environment.get("MQTT_USER") ?? ""
+        let password = Environment.get("MQTT_PASSWORD") ?? ""
+        let useTLS = (Environment.get("MQTT_TLS") ?? "false").lowercased() == "true"
 
         app.logger.notice("Mencoba terhubung ke MQTT Broker: \(host):\(port) (TLS: \(useTLS))...")
 
@@ -41,7 +41,6 @@ final class MQTTManager {
                 try await mqttClient.connect()
 
                 app.logger.notice("✅ Berhasil terhubung ke MQTT Broker!")
-
                 app.logger.notice("🔄 Mencoba subscribe ke topic: \(MQTTTopics.telemetry)")
 
                 let subscription = MQTTSubscription(
@@ -54,16 +53,15 @@ final class MQTTManager {
                 app.logger.notice("✅ BERHASIL SUBSCRIBE")
                 app.logger.notice("📡 Topic: \(MQTTTopics.telemetry)")
                 app.logger.notice("📡 Result: \(String(describing: subscribeResult))")
-
                 app.logger.notice("👂 Mulai menunggu MQTT message...")
 
                 for await message in mqttClient.messages {
-
                     app.logger.notice("🚨 MQTT MESSAGE MASUK!")
                     app.logger.notice("Topic: \(message.topic)")
+                    
                     let topic = message.topic
-
                     let data: Data
+                    
                     switch message.payload {
                     case .empty:
                         data = Data()
